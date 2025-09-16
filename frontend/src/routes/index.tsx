f881e2af316e8b433c35a2219704216b67015e92
Jonas Nilsberg Refsnæs <jonas.nilsberg.refsnes@intility.no>
2025-09-16 21:47:18 +0200

    chore: add tailwind catalyst

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
 import pb from '../pocketbase'
+import { Button } from '../components/catalyst/button'
+import { Textarea } from '../components/catalyst/textarea'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -21,6 +23,8 @@ function App() {
   return (
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+      <Textarea />
+      <Button>Testing</Button>
         <img
           src={logo}
           className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
