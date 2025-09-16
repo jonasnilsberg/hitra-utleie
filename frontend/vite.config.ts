3cd26a61edf6d28f28104605313d0eb6c11aa400
Jonas Nilsberg Refsnæs <jonas.nilsberg.refsnes@intility.no>
2025-09-16 21:28:58 +0200

    chore: update env logic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig  } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import {tanstackRouter} from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
   return {
     plugins: [
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  define: {
-    'process.env.REACT_APP_POCKETBASE_URL': env.REACT_APP_POCKETBASE_URL,
-  },
   resolve: {},
 }})
